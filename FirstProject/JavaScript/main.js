0d3ad7a27da9bc0180940f161410955055115d56
DevJaepaL <wocks3254@gmail.com>
2022-06-20 02:24:53 +0900

    22-06-20( 자바스크립트 추가, 첫번째 프로젝트 수정)

---
+++ FirstProject/JavaScript/main.js
@@ -0,0 +1,34 @@
+/* FirstProject 부분 자바스크립트 */
+
+
+function init(){
+    // 탑 스크롤 버튼 부분 변수 및 함수 선언
+    scrollBtnTop = document.getElementById('scrollButtonTop');
+    scrollBtnTop.addEventListener('click',function(){
+    window.scrollTo(0,0);
+    });
+
+    // 바텀 스크롤 버튼 부분 변수 및 함수 선언
+    scrollBtnBtm = document.getElementById('scrollButtonDown');
+    scrollBtnBtm.addEventListener('click',function(){
+    // window.scrollTo(0,document.body.scrollHeight);
+    window.scrollBy(0,1000); // 한번에 밑으로 스크롤 하지 않고 y 좌표가 1000만큼 이동하게 변경.
+    });
+
+    // 다크모드 토글 부분 변수 및 함수 선언
+    let currentCSS = document.getElementById("changeUI");
+    let darkToggle = document.getElementById("darkModeToggle"); 
+    darkToggle.addEventListener("click",function(){
+        if(currentCSS){
+            currentCSS.href = ('CSS/darkMode.css');
+        }
+    })
+
+    // 라이트모드 토글 부분 변수 및 함수 선언
+    let lightToggle = document.getElementById("lightModeToggle");
+    lightToggle.addEventListener("click",function(){
+        if(currentCSS){
+            currentCSS.href = ('CSS/style.css');
+        }
+    })
+}
--- Project2/JavaScript/indexJS.js
+++ Project2/JavaScript/indexJS.js
@@ -2,12 +2,12 @@
 
 /* 페이지 실행 시 호출 되는 함수 */
 function init() {
-
-    /* 시간,요일별 백그라운드 변경 부분 */
+    setInterval(Timer, 1000); // Timer 함수가 1초마다 실행 된다.
     let videoName = "";
     let today = new Date();
     let hours = today.getHours();
     let day = today.getDay();
+    /* 시간,요일별 UI 변경 부분 */
     // 주말일 경우
     if(0 == day % 7 || 6 == day % 7 ) {
             videoName = "Media/index_weekend.mp4";
@@ -20,7 +20,7 @@ function init() {
                 videoName = "Media/index_sun.mp4";
             }
             
-            // 시간대가 3시~ 19시 까지 일떄
+            // 시간대가 오후 3시~ 19시 까지 일떄
             else if(hours >= 15 && hours <= 19){
                 videoName = "Media/index_road.mp4";
             }
@@ -28,6 +28,17 @@ function init() {
             // 시간대가 20시 ~ 아침 6시 일때
             else{
                 videoName = "Media/index_night.mp4";
+
+                let button = document.getElementById("button");
+                button.style.filter = "brightness(-1%) invert(1)";
+
+                button.addEventListener("mouseover", function(){
+                    button.style.filter = "brightness(1) invert(0)";
+                });
+                button.addEventListener("mouseout", function(){
+                    button.style.filter = "brightness(-1%) invert(1)";
+                });
+
             }
         }
         document.getElementById("bg-video").src = videoName;
@@ -35,7 +46,29 @@ function init() {
     // 버튼 출력 부분
     let button = document.getElementById("button");
     button.addEventListener("click",function(){
-        alert("아래 페이지 부분은 미완성 됐습니다.");
+        alert("아래 페이지는 미완성 됐습니다.");
     });
+}
 
+/* 휠 아래로 굴렸을 때 메세지 출력되는 함수. */
+function wheelDown(e, obj){
+    if(e.wheelDelta < 0){
+        alert("아래 페이지는 미완성 됐습니다.");
+    }
+}
+
+/* 실시간 시계 출력 함수 */
+function Timer(){
+    const timer = document.querySelector('.timer');
+    const time = new Date();
+    const hour = time.getHours();
+    const min = time.getMinutes();
+    const sec = time.getSeconds();
+    
+    /*  삼항연산자를 통해 각 시,분,초가 10보다 작다면 앞에 0을 붙여서
+        시계를 예쁘게 출력되게 설정. */
+    timer.innerHTML = 
+    `${hour<10 ? `0${hour}`:hour}:
+    ${min<10 ? `0${min}`:min}:
+    ${sec<10 ? `0${sec}`:sec}`
 }
